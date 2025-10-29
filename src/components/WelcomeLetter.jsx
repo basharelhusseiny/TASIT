4f88ee0fc77b86ac59b7156c6d2224da770931b7
Bashar ElHusseiny <basharelhusseiny@gmail.com>
2025-10-29 19:56:52 +0300

    last

--- src/components/WelcomeLetter.jsx
+++ src/components/WelcomeLetter.jsx
@@ -4,7 +4,7 @@ const WelcomeLetter = () => {
       <p className="capitalize text-[#393941] text-[20px] font-medium pb-6">
         log in
       </p>
-      <p className="text-[46px] text-[#2E2E34] font-bold">Hi! Welcome back,</p>
+      <p className="text-[46px] text-[#2E2E34] font-bold mb-2">Hi! Welcome back,</p>
       <p className="text-[#393941] text-[33px] font-light">We’ve been missed</p>
     </div>
   );
